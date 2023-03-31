dd15a753d4c55bc4d389217872f1b5f29b14c84f
Seokyung <xuejd17@gmail.com>
2023-03-31 23:31:51 +0900

    Learn Redux Actions

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
 import { legacy_createStore as createStore } from "redux";
 
-const countModifier = (state = 0) => {
-	console.log(state);
-	return state + 1;
+const countModifier = (count = 0, action) => {
+	console.log(count, action);
+	if (action.type === "ADD") {
+		return count + 1;
+	} else if (action.type === "SUB") {
+		return count - 1;
+	} else {
+		return count;
+	}
 };
+
 const countStore = createStore(countModifier);
 
 const plus = document.getElementById("add");
 const minus = document.getElementById("sub");
 const number = document.querySelector("span");
+
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "SUB" });
 
 console.log(countStore.getState());
