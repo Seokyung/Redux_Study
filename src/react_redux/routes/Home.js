b147244a587d3b8479197a59d77088d0a806affc
Seokyung <xuejd17@gmail.com>
2023-04-01 22:27:02 +0900

    Setup for learning React-Redux

--- src/react_redux/routes/Home.js
+++ src/react_redux/routes/Home.js
@@ -1,9 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Home() {
+	const [text, setText] = useState("");
+
+	const onTextChange = (e) => {
+		const {
+			target: { value },
+		} = e;
+		setText(value);
+	};
+
+	const onAddTodo = (e) => {
+		e.preventDefault();
+		console.log(text);
+		setText("");
+	};
+
 	return (
 		<div>
 			<h2>Home</h2>
+			<form onSubmit={onAddTodo}>
+				<input
+					type="text"
+					value={text}
+					onChange={onTextChange}
+					placeholder="Write something to do!"
+				/>
+				<button>Add</button>
+			</form>
+			<ul></ul>
 		</div>
 	);
 }
