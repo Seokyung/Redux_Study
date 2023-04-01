e86ab849971bda5168afa534d2a871db3a17a14a
Seokyung <xuejd17@gmail.com>
2023-04-01 22:18:24 +0900

    Setup for learning React-Redux

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./react_redux/App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
