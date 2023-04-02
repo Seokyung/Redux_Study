9de177289c6b0dfe4a12e278bb707a5816c5802f
Seokyung <xuejd17@gmail.com>
2023-04-02 21:51:37 +0900

    Learn Redux-Toolkit createReducer() and builder callback notation

--- src/react_redux/store.js
+++ src/react_redux/store.js
@@ -1,5 +1,5 @@
 import { legacy_createStore as createStore } from "redux";
-import { createAction } from "@reduxjs/toolkit";
+import { createAction, createReducer } from "@reduxjs/toolkit";
 
 const addTodo = createAction("ADD");
 const deleteTodo = createAction("DELETE");
@@ -16,16 +16,36 @@ const deleteTodo = createAction("DELETE");
 // 	return { type: DELETE, id: parseInt(id) };
 // };
 
-const reducer = (state = [], action) => {
-	switch (action.type) {
-		case addTodo.type:
-			return [{ text: action.payload, id: Date.now() }, ...state];
-		case deleteTodo.type:
+const reducer = createReducer([], (builder) => {
+	builder
+		.addCase(addTodo, (state, action) => {
+			state.push({ text: action.payload, id: Date.now() });
+		})
+		.addCase(deleteTodo, (state, action) => {
 			return state.filter((todo) => todo.id !== action.payload);
-		default:
-			return state;
-	}
-};
+		});
+});
+
+// object notation for createReducer() is deprecated, use builder callback notation
+// const reducer = createReducer([], {
+// 	[addTodo]: (state, action) => {
+// 		state.push({ text: action.payload, id: Date.now() });
+// 	},
+// 	[deleteTodo]: (state, action) =>
+// 		state.filter((todo) => todo.id !== action.payload),
+// });
+
+// createReducer() covers this code
+// const reducer = (state = [], action) => {
+// 	switch (action.type) {
+// 		case addTodo.type:
+// 			return [{ text: action.payload, id: Date.now() }, ...state];
+// 		case deleteTodo.type:
+// 			return state.filter((todo) => todo.id !== action.payload);
+// 		default:
+// 			return state;
+// 	}
+// };
 
 const store = createStore(reducer);
 
